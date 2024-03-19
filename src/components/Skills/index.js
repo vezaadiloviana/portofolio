10a5f4ab3c54ea7017b3cc662faf0cfb9456c2c7
vezaadiloviana <vezaadiloviana27@gmail.com>
2024-03-19 14:44:15 +0700

    Skills & Capabilities

---
+++ src/components/Skills/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Title from "../Title";
+import imghtml from "../../assets/img/skills/html.png"
+import imgcss from "../../assets/img/skills/css.png"
+import imgreact from "../../assets/img/skills/react.png"
+import imglaravel from "../../assets/img/skills/laravel.png"
+import imgtailwind from "../../assets/img/skills/tailwind.png"
+import imggithub from "../../assets/img/skills/github.png"
+
+const Skills = () => {
+  return (
+    <div id="skills" className="bg-[url('./assets/img/bg-skills.png')] bg-cover bg-center p-10 -mx-10">
+        <div className="flex justify-center">
+            <Title title={"Skills &"} subTitle={"Capabilities"}/>
+        </div>
+        <div className="py-5 flex flex-wrap justify-center gap-5 md:gap-10">
+            <img className="h-10 md:h-24" src={imghtml} alt="HTML" />
+            <img className="h-10 md:h-24" src={imgcss} alt="CSS" />
+            <img className="h-10 md:h-24" src={imgreact} alt="React" />
+            <img className="h-10 md:h-24" src={imglaravel} alt="Laravel" />
+            <img className="h-10 md:h-24" src={imgtailwind} alt="Tailwind" />
+            <img className="h-10 md:h-24" src={imggithub} alt="GitHub" />
+        </div>
+      
+    </div>
+  );
+};
+
+export default Skills;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import NavbarApp from '../../components/NavbarApp'
 import Jumbotron from '../../components/Jumbotron'
 import About from '../../components/About'
+import Skills from '../../components/Skills'
 
 function Home() {
   return (
     <div className='bg-lightMode dark:bg-darkMode'>
         <NavbarApp/>
         <Jumbotron />
-        <div className='p-10'>
+        <div className='px-10 pt-10'>
             <About />
+            <Skills />
         </div>
         <footer className="bg-primaryBlue text-white text-base w-full max-w-[1600px] text-center px-10 py-4" >
           The page is still under development <br/>
