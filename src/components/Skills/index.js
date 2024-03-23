d3bf6ea1cd03f7bc2621e22da948e3d423b7a580
vezaadiloviana <vezaadiloviana27@gmail.com>
2024-03-23 10:41:43 +0700

    update

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -6,6 +6,7 @@ import imgreact from "../../assets/img/skills/react.png"
 import imglaravel from "../../assets/img/skills/laravel.png"
 import imgtailwind from "../../assets/img/skills/tailwind.png"
 import imggithub from "../../assets/img/skills/github.png"
+import imgjs from '../../assets/img/skills/js.png'
 
 const Skills = () => {
   return (
@@ -16,6 +17,7 @@ const Skills = () => {
         <div className="py-5 flex flex-wrap justify-center gap-5 md:gap-10">
             <img className="h-10 md:h-24" src={imghtml} alt="HTML" />
             <img className="h-10 md:h-24" src={imgcss} alt="CSS" />
+            <img className="h-10 md:h-24" src={imgjs} alt="JS" />
             <img className="h-10 md:h-24" src={imgreact} alt="React" />
             <img className="h-10 md:h-24" src={imglaravel} alt="Laravel" />
             <img className="h-10 md:h-24" src={imgtailwind} alt="Tailwind" />
